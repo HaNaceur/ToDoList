59d7df0a6ca79ef928e5210f524ed2d430f6a938
Hamza Naceur <hamzanaceur@MacBook-Air-de-Hamza.local>
2022-12-11 14:21:29 +0100

    chore: installyarn webpack

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+// == Import
+import reactLogo from './react-logo.svg';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="app">
+      <img src={reactLogo} alt="react logo" />
+      <h1>Coucou les kelvin</h1>
+      <h2>Bon courage pour votre projet !</h2>
+    </div>
+  );
+}
+
+// == Export
+export default App;
