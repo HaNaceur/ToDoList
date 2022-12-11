ec7170a3fa2b7831b97b676cb312ae271ad651d2
Hamza Naceur <hamzanaceur@MacBook-Air-de-Hamza.local>
2022-12-11 16:19:36 +0100

    feat: form input onSubmit

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,7 +1,5 @@
 /* eslint-disable react/prefer-stateless-function */
 // == Import
-
-import Main from './Main/Main'
 import data from '../../data/tasks';
 
 import './styles.scss';
@@ -13,6 +11,20 @@ class App extends React.Component {
     this.state = {
     tasks: data,
     };
+
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
   }
 
   componentDidMount(){
@@ -29,7 +41,6 @@ class App extends React.Component {
     // Update state with incremented value
     setCount(count + 1);
   };
-  const {tasks} = this.state;
   const [checked, setChecked] = React.useState(false);
   const placeholder = 'Add a task';
 
@@ -39,9 +50,13 @@ const handleChange = () => {
 
     return (
         <div className="app">
-        <form className="form">
+        <form className="form" onSubmit={this.handleSubmit}>
+          <label>
          <input type="text" className="form-item" placeholder={placeholder}
+         value={this.state.value} onChange={this.handleChange}
          />
+         <input type="submit" value="Submit" />
+         </label>
         </form>
        
         <p className="counter">{count} Tâches en cour {count > 1 && 's'}</p>
